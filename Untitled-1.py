40b33b08c2f8898b16073d08c922530036127851
Irene <xxiellan@gmail.com>
2024-07-18 22:09:45 -0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+name1 = 'Rebecca'
+name2 = 'Gerry'
+name3 = 'Doubao'
+
+print('my name is ' + name1)
+print('his name is ' + name2)
+print('cat name is ' + name3)
+
+
+print('we are ' + name1 + ', ' + name2 + ' and ' + name3)
